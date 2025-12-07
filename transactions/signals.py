ac626ffcfa2e90eee55c7e0220f97609ab39c051
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-12-07 16:25:47 +0100

    signals

--- transactions/signals.py
+++ transactions/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import (post_save,post_delete,
                                       pre_delete,pre_save)
 from django.dispatch import receiver
-from transactions.models import Transaction
+from django.db import models
+from transactions.models import Payment, Transaction
 from users.models import Customer
 
 #  date = models.DateField(auto_now_add=True)
@@ -33,46 +34,54 @@ from users.models import Customer
 
 @receiver(pre_save, sender=Transaction)
 def create_transaction(sender, instance, **kwargs):
-
     customer = None
+    old = None
     try:
         customer = Customer.objects.get(pk=instance.customer.id)
     except Customer.DoesNotExist:
         return
     
-    naira = round((instance.amount * instance.naira_rate),2)
-    balance = round((instance.paid_amount - naira),2)
-    instance.balance = balance
-    print("PAID AMOUNT:", instance.paid_amount)
-    print("NAIRA AMOUNT:", naira)
-    print("BALANCE BEFORE SAVE SIGNAL:", balance)
+    if instance.pk:
+        old = Transaction.objects.get(pk = instance.pk)
 
 
-    if balance < 0:
-        instance.paid_once = False
+    instance.naira = round((instance.amount * instance.naira_rate),2)     
+    instance.usd_ask = round((instance.amount / instance.usd_rate),2)
+    instance.usd_bid = round((instance.amount / instance.usd_price),2)
+    instance.usd_gain = round((((1/instance.usd_price) - (1/instance.usd_rate)) * instance.amount),2)
 
 
     if not instance.pk:
         transaction_id = "kjkjkjk"  # Replace with actual logic to generate transaction ID
         instance.transaction_id = transaction_id
+        balance = round((instance.paid_amount - instance.naira),2)
+        instance.balance = balance
+        customer.balance += balance
+        customer.save()
+    elif instance.pk:
+        # check if naira or paid_amount changed
+        if (old.paid_amount != instance.paid_amount) or (old.naira != instance.naira):
+            total_payments = Payment.objects.filter(transaction=instance).aggregate(total=models.Sum('amount'))['total'] or 0.0
+            # print("old", old.paid_amount)
+            # print("new",instance.paid_amount)
+            # print("BALANCE",instance.balance)
+            instance.balance = round((instance.paid_amount - instance.naira) + total_payments,2)
+            # print("updated",instance.balance)
+
+        # check if the balance was edited
+        if old.balance != instance.balance:
+            # balance edited
+            diff = instance.balance - old.balance
+            # print("diff",diff)
+            customer.balance += diff
+            customer.save()
+
+    if instance.balance < 0 and not instance.pk:
+        instance.paid_once = False
+
         
-    instance.naira = naira
-    instance.usd_ask = round((instance.amount / instance.usd_rate),2)
-    instance.usd_bid = round((instance.amount / instance.usd_price),2)
-    instance.usd_gain = round((((1/instance.usd_price) - (1/instance.usd_rate)) * instance.amount),2)
 
-    if instance.pk:
-        old = Transaction.objects.get(pk = instance.pk)
-        customer.balance -= old.balance
-        customer.balance += balance
-        customer.save()
-    else:
-        customer.balance += balance
-        customer.save()
-
-    # instance.save()
-
-@receiver(post_delete, sender=Transaction)
+@receiver(pre_delete, sender=Transaction)
 def delete_transaction(sender, instance, **kwargs):
     customer = None
     try:
@@ -80,5 +89,35 @@ def delete_transaction(sender, instance, **kwargs):
     except Customer.DoesNotExist:
         return
     
-    customer.balance -= instance.balance
+    total_payments = Payment.objects.filter(transaction=instance).aggregate(total=models.Sum('amount'))['total'] or 0.0
+    customer.balance -= instance.balance + total_payments
     customer.save()
+
+
+@receiver(pre_save, sender=Payment)
+def create_payment(sender,instance,**kwargs):
+    transaction = Transaction.objects.get(pk = instance.transaction.id)
+
+    if instance.pk:
+        old = Payment.objects.get(pk = instance.pk)
+
+        if old.amount != instance.amount:
+            diff = instance.amount - old.amount
+            transaction.balance += diff
+            transaction.save()
+    else:
+        transaction.balance += instance.amount
+        transaction.save()
+
+@receiver(post_delete, sender=Payment)
+def delete_payment(sender, instance, **kwargs):
+    transaction = None
+    try:
+        transaction = Transaction.objects.get(pk=instance.transaction.id)
+        print("transaction found")
+    except Transaction.DoesNotExist:
+        return
+
+    transaction.balance += instance.amount
+    transaction.save()
+
